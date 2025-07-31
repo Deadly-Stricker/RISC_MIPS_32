f5e6dfba461fdc9d5299ceff9a2fcaa1cda99d21
Deadly-Stricker <sakshamsinha143@gmail.com>
2025-07-31 21:18:08 +0530

    createtd and edited Instruction registor but didnt gave values to instruction register

---
+++ InstructionRegister.v
@@ -0,0 +1,10 @@
+module instructionRegister(
+    input clk,
+    input [5:0]pc,
+    output reg [31:0]instruction
+);
+    reg [31:0] Instruction_Register [15:0];
+    always @(posedge clk ) begin
+        instruction = Instruction_Register[pc];
+    end
+endmodule
