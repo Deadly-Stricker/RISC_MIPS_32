617b250cb6c92d6c1cc58088ba8f00fde767365a
Deadly-Stricker <sakshamsinha143@gmail.com>
2025-08-01 12:12:02 +0530

    Included control signals in the declaration of the register files

--- ID/registerFile.v
+++ ID/registerFile.v
@@ -1,11 +1,15 @@
 module registerFile(
     input [4:0] addrss,
     input clk,
+    input read,
+    input write,
+    input [31:0] write_material,
     output reg [31:0] Outp
 );
     reg [31:0] Register_File [31:0];
     always @(posedge clk ) begin
-        Outp <= Register_File[addrss];
+        if(read)   Outp <= Register_File[addrss];
+        else if(write)  Register_File[addrss]=write_material;    
     end
 
 endmodule
