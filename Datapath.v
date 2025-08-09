5812af4565705215c2e9d6f37545d2bb01cd66b1
Saksham <sakshamsinha143@gmail.com>
2025-08-09 17:51:41 +0530

    just resolved multiple declarations

--- Datapath.v
+++ Datapath.v
@@ -2,10 +2,11 @@ module datapath(
     input clk1,
     input clk2,
     input clk,
+    output [31:0] out1A
 );
     wire stall,jump_cs;
     wire [31:0] instruction,next_pc;
-    reg [31:0] out1A;
+    // reg [31:0] out1A;
     IF_stage a(clk,stall,next_pc,jump_cs,instruction);
     Rego A(clk1,instruction,out1A);
     ID_stage b(clk,out1A);
