85cef290f5da8e4412a088b38e32a90a46b4e445
Saksham <sakshamsinha143@gmail.com>
2025-08-13 15:29:24 +0530

    Absolute path used

--- IF/InstructionRegister.v
+++ IF/InstructionRegister.v
@@ -7,6 +7,7 @@ module instructionRegister(
     integer i;
     initial begin
         //read from a mem file
+        $readmemh("/home/saksham/Codes/RISC_MIPS_32/IF/content_IF.mem",Instruction_Register);
     end
     always @(posedge clk ) begin
         instruction = Instruction_Register[pc];
