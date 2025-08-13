683ff47f74b47a95ba59c6dd50ba1b32bb38ca0a
Saksham <sakshamsinha143@gmail.com>
2025-08-13 14:43:38 +0530

    changed IF stage a bit

--- IF/if_stage.v
+++ IF/if_stage.v
@@ -3,9 +3,9 @@ module IF_stage(
     input stall,
     input [31:0]Next_pc,
     input jump_cs,
-    output reg [31:0]instruction
+    output [31:0]instruction
 );
-    wire connector_from_pc_to_IR; 
+    wire [31:0] connector_from_pc_to_IR; 
     // reg Instruction_storage;
     programCounter PC_Module (.clk(clk),.stall(stall),.Next_pc(Next_pc),.jump_cs(jump_cs),.pc_curr(connector_from_pc_to_IR));
     instructionRegister IR_Module (.clk(clk),.pc(connector_from_pc_to_IR),.instruction(instruction));
