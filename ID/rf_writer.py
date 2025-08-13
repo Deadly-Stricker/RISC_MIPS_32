1cf94545f13abf360d66ef06f1759c96d7c54faa
Saksham <sakshamsinha143@gmail.com>
2025-08-13 15:02:32 +0530

    updated path to absolute path to specify the path to the mem files

--- ID/rf_writer.py
+++ ID/rf_writer.py
@@ -1,4 +1,4 @@
-with open('content_rf.mem','w') as fi:
+with open('/home/saksham/Codes/RISC_MIPS_32/ID/content_rf.mem','w') as fi:
     data=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,15,16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
     for i in data:
         fi.write(f"{i:32X}\n")
