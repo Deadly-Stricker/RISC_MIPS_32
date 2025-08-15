8c1a650e11d1d6ab5fb24b984003c2836294c30c
Deadly-Stricker <sakshamsinha143@gmail.com>
2025-08-15 22:14:37 +0530

    updated so that i can access the cuurent pc later in the ID and ALU stage

--- IF/if_stage.v
+++ IF/if_stage.v
@@ -3,11 +3,13 @@ module IF_stage(
     input stall,
     input [31:0]Next_pc,
     input jump_cs,
-    output [31:0]instruction
+    output [31:0]instruction,
+    output [31:0] outpc
 );
     wire [31:0] connector_from_pc_to_IR; 
+    
     // reg Instruction_storage;
     programCounter PC_Module (.clk(clk),.stall(stall),.Next_pc(Next_pc),.jump_cs(jump_cs),.pc_curr(connector_from_pc_to_IR));
-    instructionRegister IR_Module (.clk(clk),.pc(connector_from_pc_to_IR),.instruction(instruction));
-
+    instructionRegister IR_Module (.pc(connector_from_pc_to_IR),.instruction(instruction));
+    assign outpc = connector_from_pc_to_IR;
 endmodule
