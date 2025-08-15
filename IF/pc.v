00b2b585dd40368c872805e580418a61afabba46
Deadly-Stricker <sakshamsinha143@gmail.com>
2025-08-15 15:54:46 +0530

    initialised pc_curr

--- IF/pc.v
+++ IF/pc.v
@@ -6,13 +6,16 @@ module programCounter (
     output reg [31:0]pc_curr    // ye chahiye baad ke liye
 );
     //reg pc_past;
+    initial begin
+        pc_curr=0;
+    end
     always @(posedge clk)
         begin
             if (stall)  pc_curr=pc_curr;
             else
                 begin
                     if(jump_cs) pc_curr=Next_pc;
-                    else    pc_curr=pc_curr+4;
+                    else    pc_curr=pc_curr+1;
                 end
             //pc_past=pc_curr;
         end
