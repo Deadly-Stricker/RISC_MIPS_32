1b2ec55943bf55066d623d3b69707d7e85fa85cc
Deadly-Stricker <sakshamsinha143@gmail.com>
2025-08-20 23:42:35 +0530

    removed clk1 and clk2 replaced them with clk

--- Datapath.v
+++ Datapath.v
@@ -1,6 +1,6 @@
 module datapath(
-    input clk1,
-    input clk2,
+    // input clk,
+    // input clk,
     input clk,
     output [31:0] outx
 );
@@ -12,8 +12,8 @@ module datapath(
     IF_stage a(clk,stall,next_pc,jump_cs,instruction,pc_val);
 
     wire [31:0] instructionl1,pc_vall1;
-    Rego32 A1(clk1,instruction,instructionl1);
-    Rego32 A2(clk1,pc_val,pc_vall1);
+    Rego32 A1(clk,instruction,instructionl1);
+    Rego32 A2(clk,pc_val,pc_vall1);
 
     wire [5:0] opcode;
     wire [4:0] rt;               // target register
@@ -67,15 +67,15 @@ module datapath(
     wire [4:0] write_address,sal3,rtl3;           //later in mem stage i will need to use this to write back to registers
     registerFile d(readadd1,readadd2,write_address,clk,write,write_material,Rs,Rt);
 
-    Rego32 B1(clk2,Rs,Rsl3);
-    Rego32 B2(clk2,Rt,Rtl3);
-    Rego32 B3(clk2,immed,immedl3);
-    Rego6 B4(clk2,opcode,opcodel3);
-    Rego32 B5(clk2,pc_val,pc_vall3);
-    Rego32 B6(clk2,outpc,outpcl3);
-    Rego6 B7(clk2,funct,functl3);
-    Rego5 B8(clk2,sa,sal3);
-    Rego5 B9(clk2,rt,rtl3);
+    Rego32 B1(clk,Rs,Rsl3);
+    Rego32 B2(clk,Rt,Rtl3);
+    Rego32 B3(clk,immed,immedl3);
+    Rego6 B4(clk,opcode,opcodel3);
+    Rego32 B5(clk,pc_val,pc_vall3);
+    Rego32 B6(clk,outpc,outpcl3);
+    Rego6 B7(clk,funct,functl3);
+    Rego5 B8(clk,sa,sal3);
+    Rego5 B9(clk,rt,rtl3);
 
     // assign outx = Rs;
     wire [31:0] Output_;
@@ -95,11 +95,11 @@ module datapath(
     wire [5:0] opcodel4;
     wire [4:0] rtl4;
     wire [31:0] Rsl4,Rtl4,Output_l4;
-    Rego6 C1(clk1,opcodel3,opcodel4);
-    Rego5 C2(clk1,rtl3,rtl4);
-    // Rego32 C2(clk1,Rsl3,Rsl4);
-    Rego32 C3(clk1,Rtl3,Rtl4);
-    Rego32 C4(clk1,Output_,Output_l4);
+    Rego6 C1(clk,opcodel3,opcodel4);
+    Rego5 C2(clk,rtl3,rtl4);
+    // Rego32 C2(clk,Rsl3,Rsl4);
+    Rego32 C3(clk,Rtl3 ,Rtl4);
+    Rego32 C4(clk,Output_,Output_l4);
 
     MEM_stage f(clk,opcodel4,rtl4,Rtl4,Output_l4,write_material,write,write_address);
     assign outx = Output_l4;
