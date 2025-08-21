2d3a9da4ffea083b91d50adce1f4d9cdacf63608
Deadly-Stricker <sakshamsinha143@gmail.com>
2025-08-21 19:22:13 +0530

    solved a afew problems

--- MEM/MEM_stage.v
+++ MEM/MEM_stage.v
@@ -1,13 +1,13 @@
 module MEM_stage(
     input clk,
     input [5:0] opcode,
-    input [4:0] Rt, //rt adress
+    // input [4:0] Rt, //rt adress
     input [31:0] Rt_value,
     input [31:0] effictiveaddr,
     // input [31:0] write_stuff,
-    output reg [31:0] op_mem,  //can be write material for register file fo sw
-    output reg write_reg,
-    output reg [4:0] Rt_address
+    output reg [31:0] op_mem  //can be write material for register file fo sw
+    // output reg write_reg,
+    // output reg [4:0] Rt_address
 );
     reg write,read;
     wire [31:0] outp_of_mem;
@@ -17,11 +17,11 @@ module MEM_stage(
         case (opcode)
             6'b100011: // lw
                 begin
-                    write_reg <= 1; // to register to start writing
+                    // write_reg <= 1; // to register to start writing
                     addrRead <= effictiveaddr; // giving effective address to memory
                     read<=1; // for the memory to start reading
                     op_mem <= outp_of_mem; //write material
-                    Rt_address <= Rt; // addrssw
+                    // Rt_address <= Rt; // addrssw
                 end
 
             6'b101011: // sw
