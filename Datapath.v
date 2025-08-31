24865aff76abcdecd71f24cd3f0a43ab67845a0e
Deadly-Stricker <sakshamsinha143@gmail.com>
2025-08-31 18:05:36 +0530

    write back is now in rd

--- Datapath.v
+++ Datapath.v
@@ -65,7 +65,7 @@ module datapath(
     wire [5:0] opcodel3,functl3;
     wire [1:0] InstructionTypel3;
     wire [31:0] write_material,pc_vall3,outpcl3, Rs,Rt,Rsl3,Rtl3,immedl3; //this too will be used to write back in mem stage later to registers
-    wire [4:0] write_address,sal3,rtl3,rsl3;           //later in mem stage i will need to use this to write back to registers
+    wire [4:0] write_address,sal3,rtl3,rsl3,rdl3;           //later in mem stage i will need to use this to write back to registers
     registerFile d(readadd1,readadd2,write_address,clk,write,write_material,Rs,Rt);
 
     Rego32 B1(clk,Rs,Rsl3);
@@ -78,6 +78,7 @@ module datapath(
     Rego5 B8(clk,sa,sal3);
     Rego5 B9(clk,rt,rtl3);
     Rego5 B11(clk,rs,rsl3);
+    Rego5 B12(clk,rd,rdl3);
     Rego2 B10(clk,InstructionType,InstructionTypel3);
 
     // assign outx = Rs;
@@ -96,14 +97,16 @@ module datapath(
     );
     // assign outx = Output_ ;
     wire [5:0] opcodel4;
-    wire [4:0] rtl4,rsl4;
+    wire [4:0] rtl4,rsl4,rdl4;
     wire [31:0] Rsl4,Rtl4,Output_l4;
     wire [1:0] InstructionTypel4;
     Rego6 C1(clk,opcodel3,opcodel4);
     Rego5 C2(clk,rtl3,rtl4);
     Rego5 C6(clk,rsl3,rsl4);
+    Rego5 C7(clk,rdl3,rdl4);
     // Rego32 C2(clk,Rsl3,Rsl4);
     Rego32 C3(clk,Rtl3 ,Rtl4);
+    // Rego32 C7(clk,Rdl3 ,Rdl4);
     Rego32 C4(clk,Output_,Output_l4);
     Rego2 C5(clk,InstructionTypel3,InstructionTypel4);
 
@@ -112,17 +115,18 @@ module datapath(
     assign outx = Output_l4;
 
     wire [5:0] opcodel5;
-    wire [4:0] rtl5,rsl5;
+    wire [4:0] rtl5,rsl5,rdl5;
     wire [1:0] InstructionTypel5;
     wire [31:0] writedata;
 
     Rego6 D1(clk,opcodel4,opcodel5);
     Rego5 D2(clk,rtl4,rtl5);
     Rego5 D3(clk,rsl4,rsl5);
+    Rego5 D6(clk,rdl4,rdl5);
     Rego2 D4(clk,InstructionTypel4,InstructionTypel5);
     Rego32 D5(clk,Output_l4,writedata);
 
-    wb_stage g(clk,opcodel5,InstructionTypel5,rsl5,rtl5,writedata,write_material1,write_address,write,write_material);
+    wb_stage g(clk,opcodel5,InstructionTypel5,rdl5,rtl5,writedata,write_material1,write_address,write,write_material);
 
 
 endmodule
