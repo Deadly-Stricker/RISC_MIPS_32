518c3f8e45f2daf1b856ec080407c95eee8aca98
Deadly-Stricker <sakshamsinha143@gmail.com>
2025-09-13 13:24:57 +0530

    updated the definition of these modules to incorperate stalls as gate keeper

--- registers.v
+++ registers.v
@@ -1,30 +1,30 @@
-module Rego32(input clk,input [31:0] in,output reg [31:0] out);
+module Rego32(input clk,input [31:0] in,input stall,output reg [31:0] out);
 always @(posedge clk ) begin
-    out <= in;
+    out <=stall ?  out:in;
 end
 endmodule
-module Rego6(input clk,input [5:0] in,output reg [5:0] out);
+module Rego6(input clk,input [5:0] in,input stall,output reg [5:0] out);
 always @(posedge clk ) begin
-    out <= in;
+    out <=stall ?  out:in;
 end
 endmodule
-module Rego5(input clk,input [4:0] in,output reg [4:0] out);
+module Rego5(input clk,input [4:0] in,input stall,output reg [4:0] out);
 always @(posedge clk ) begin
-    out <= in;
+    out <=stall ?  out:in;
 end
 endmodule
-module Rego15(input clk,input [14:0] in,output reg [14:0] out);
+module Rego15(input clk,input [14:0] in,input stall,output reg [14:0] out);
 always @(posedge clk ) begin
-    out <= in;
+    out <=stall ?  out:in;
 end
 endmodule
-module Rego26(input clk,input [25:0] in,output reg [25:0] out);
+module Rego26(input clk,input [25:0] in,input stall,output reg [25:0] out);
 always @(posedge clk ) begin
-    out <= in;
+    out <=stall ?  out:in;
 end
 endmodule
-module Rego2(input clk,input [1:0] in,output reg [1:0] out);
+module Rego2(input clk,input [1:0] in,input stall,output reg [1:0] out);
 always @(posedge clk ) begin
-    out <= in;
+    out <=stall ?  out:in;
 end
 endmodule
